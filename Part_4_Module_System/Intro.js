85f2bfa688171682565f63783348686fdca443e9
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-02 11:29:49 +0530

    Intro to async

---
+++ Part_4_Module_System/Intro.js
@@ -0,0 +1,22 @@
+// The process.argv working example ---------------------------------------------------//
+
+/*
+const mission = process.argv[2];
+
+if (mission === 'learn') {
+    console.log('Time to write some Node code!');
+} else {
+    console.log(`Is ${mission} really more fun?`);
+}
+*/
+
+// The example of asysnchronous function --------------------------------------------//
+
+console.log("rabbit finished!");
+console.log("Tortoise finished!");
+
+// The example of asysnchronous function --------------------------------------------//
+
+setTimeout(() => console.log('rabbit finished'), 1000);
+setTimeout(() => console.log('Tortoise finished'), 500);
+
