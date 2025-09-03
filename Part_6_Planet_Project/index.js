acd44367d7cdf586a4c922322a0115f6096ac8f6
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-03 12:07:59 +0530

    Planet project part completed

--- Part_6_Planet_Project/index.js
+++ Part_6_Planet_Project/index.js
@@ -1,3 +1,5 @@
+//--------------------------------------------- PLANET PROJECT -----------------------------------------------------//
+
 
 // Importing the csv-parse package that we just installed
 const { parse } = require('csv-parse');
@@ -5,8 +7,18 @@ const { parse } = require('csv-parse');
 // Importing the file system package that is inbuilt in the node
 const fs = require('fs');
 
-//Creating an array for the data
+// Creating an array for the data
 const results = [];
+
+// Creating a new array that will store only those planets that have the capability of life
+const habitablePlanets = [];
+
+// Creating a new function to check if the planet is habitable or not 
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 // creating a readstream for the data of the kepler_data.csv file
 fs.createReadStream('./kepler_data.csv')
@@ -18,6 +30,11 @@ fs.createReadStream('./kepler_data.csv')
     // creating the first event if data is recieved
     .on('data', (data) => {
         results.push(data);
+        
+        // check if the planet is habitable or not 
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     // creating another event if any type of error comes
     .on('error', (err) => {
@@ -27,5 +44,12 @@ fs.createReadStream('./kepler_data.csv')
     .on('end', () => {
         console.log(results);
         console.log('Done processing the file');
+
+        // Printing the number of habitable planets 
+        console.log(`The number of habitable planets are: ${habitablePlanets.length}`);
+        for (let i = 0; i < habitablePlanets.length; i++) {
+            console.log(habitablePlanets[i].kepler_name)
+        }
     });
 
+// ----------------------------------------------- COMPLETED -------------------------------------------------------//
