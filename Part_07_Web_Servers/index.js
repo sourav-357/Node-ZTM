2b5882a2c20750c23e49b770bd839f921c6a594a
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-03 13:04:09 +0530

    creating a new server

---
+++ Part_07_Web_Servers/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "content-type": 'application/json',
+    })
+    res.end(JSON.stringify({
+        id: 1,
+        name: 'Sir Issac Newton',
+    }));
+});
+
+server.listen(PORT, () => {
+    console.log(`Listining to port ${PORT}`)
+});
+
