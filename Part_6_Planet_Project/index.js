a14c09d28076831e8a2566319a129cc9c2f42290
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-03 11:07:35 +0530

    creating a readstream for the data of the kepler_data.csv file

---
+++ Part_6_Planet_Project/index.js
@@ -0,0 +1,26 @@
+
+// Importing the csv-parse package that we just installed
+const { parse } = require('csv-parse');
+
+// Importing the file system package that is inbuilt in the node
+const fs = require('fs');
+
+//Creating an array for the data
+const results = [];
+
+// creating a readstream for the data of the kepler_data.csv file
+fs.createReadStream('./kepler_data.csv')
+    // creating the first event if data is recieved
+    .on('data', (data) => {
+        results.push(data);
+    })
+    // creating another event if any type of error comes
+    .on('error', (err) => {
+        console.log(err);
+    })
+    // creating another event if the file is completed
+    .on('end', () => {
+        console.log(results);
+        console.log('Done processing the file');
+    });
+
