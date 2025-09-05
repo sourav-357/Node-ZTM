528482d3d5762b8c9ace2319b8a22286e82286d6
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-05 22:49:00 +0530

    sendFile() function in use

--- Part_08_Express_API/controllers/messages.controller.js
+++ Part_08_Express_API/controllers/messages.controller.js
@@ -1,8 +1,12 @@
 
+// Importing a module to use the sendFile() function 
+const path = require('path');
+
 // Function to show the message when the url is what we want 
 function getMessages(req, res) {
     // responding to the requested get url by res.send() function
-    res.send('<ul><li>Helooooo Riyanshu....</li></ul>');
+    // res.send('<ul><li>Helooooo Riyanshu....</li></ul>');
+    res.sendFile(path.join(__dirname, '../public/skimountain.jpg'));
 }
 
 // Function where we will post any message 
