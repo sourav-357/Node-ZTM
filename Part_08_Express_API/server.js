3e46735d3dd5178aa978d1254380ab8a76d1371d
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-05 09:13:54 +0530

    Routing our first express server

--- Part_08_Express_API/server.js
+++ Part_08_Express_API/server.js
@@ -10,6 +10,32 @@ const app = express();
 // Setting up the port number -->> 3000
 const PORT = 3000;
 
+// Routing the Express Server to any particular url
+app.get('/', (req, res) => {
+
+    // Directly passing the work that it will do without even telling the res.setHeader('text/html') function
+    // Without even passing the stringify object or res.end() function
+    res.send({
+        roll: '2409146',
+        name: 'Sourav Kumar',
+        semester: '2nd Semester',
+        branch: 'Computer Science and Engineering',
+    });
+});
+
+// Routing another url in express
+app.get('/messages', (req, res) => {
+    
+    // responding to the requested get url by res.send() function
+    res.send('<ul><li>Helooooo Riyanshu....</li></ul>');
+});
+
+// Routing another url in express 
+app.post('/messages', (req, res) => {
+    console.log('Updateing messages...');
+});
+
+// Starting the port at 3000 by ".listen(port, () => {})" function 
 app.listen(PORT, () => {
     console.log(`Listining to server at port ${PORT}...`)
 });
