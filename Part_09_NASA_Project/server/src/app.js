a104678966ac644885fa2ab7fb99e8a863e879d2
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-06 13:30:17 +0530

    Creating server with help of http and express at the same time

---
+++ Part_09_NASA_Project/server/src/app.js
@@ -0,0 +1,9 @@
+
+// Importing the express module from express that we installed
+const express = require('express');
+
+// creating a server by help of express 
+const app = express();
+
+// exporting this module to server.js
+module.exports = app;
--- Part_09_NASA_Project/server/src/server.js
+++ Part_09_NASA_Project/server/src/server.js
@@ -2,11 +2,16 @@
 // Importing the http module
 const http = require('http');
 
+// Importing the express module from app.js 
+const app = require('./app.js');
+
 // Setting up the port number
 const PORT = process.env.PORT || 8000; // This will check if there is default environment port and if not then -->> 8000
 
 // Creating a server from http module
-const server = http.createServer( /*Some parameters*/ );
+// We know that http module needs a (req and res) parameter and a callback function, but all of them are under the express server called app that we pass as parameters
+// We are basically adding the express on top of http server
+const server = http.createServer(app);
 
 // Starting the server 
 server.listen(PORT, () => {
