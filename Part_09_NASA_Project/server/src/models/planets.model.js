3c787d3e2cc0b9f15353f4336265ca7032e9b4de
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-06 17:10:51 +0530

    Async await untill the data is read completely from the csv file

--- Part_09_NASA_Project/server/src/models/planets.model.js
+++ Part_09_NASA_Project/server/src/models/planets.model.js
@@ -21,39 +21,50 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
-// creating a readstream for the data of the kepler_data.csv file
-fs.createReadStream('./kepler_data.csv')
-    // parsing the data recieved from kepler file then proceeding it further to data event
-    .pipe(parse({
-        comment: '#',
-        columns: true,
-    }))
-    // creating the first event if data is recieved
-    .on('data', (data) => {
-        results.push(data);
-        
-        // check if the planet is habitable or not 
-        if (isHabitablePlanet(data)) {
-            habitablePlanets.push(data);
-        }
-    })
-    // creating another event if any type of error comes
-    .on('error', (err) => {
-        console.log(err);
-    })
-    // creating another event if the file is completed
-    .on('end', () => {
-        console.log(results);
-        console.log('Done processing the file');
+// Creating a function to load planets data 
+function loadPlanetsData() {
 
-        // Printing the number of habitable planets 
-        console.log(`The number of habitable planets are: ${habitablePlanets.length}`);
-        for (let i = 0; i < habitablePlanets.length; i++) {
-            console.log(habitablePlanets[i].kepler_name)
-        }
+    // Creating a new promise so that the process moves ahead when the data is read completely
+    return new Promise((resolve, reject) => {
+
+        // creating a readstream for the data of the kepler_data.csv file
+        fs.createReadStream('./kepler_data.csv')
+            // parsing the data recieved from kepler file then proceeding it further to data event
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+            // creating the first event if data is recieved
+            .on('data', (data) => {
+                results.push(data);
+                
+                // check if the planet is habitable or not 
+                if (isHabitablePlanet(data)) {
+                    habitablePlanets.push(data);
+                }
+            })
+            // creating another event if any type of error comes
+            .on('error', (err) => {
+                console.log(err);
+
+                // we need to reject the promise when error comes 
+                reject(err);
+            })
+            // creating another event if the file is completed
+            .on('end', () => {
+                // Printing the number of habitable planets 
+                console.log(`The number of habitable planets are: ${habitablePlanets.length}`);
+                
+                // resolving the promise to move further
+                resolve();
+            });
     });
+}
+
 
 // Exporting the module data now so that it could be used by the planet.controller
 module.exports = {
-    
+    loadPlanetsData,
+    planets: habitablePlanets,
 }
+
