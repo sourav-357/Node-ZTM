acdf05b14a20a1ebe871b3f0ecc478f299352e8d
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-06 13:33:06 +0530

    using the express.json() to parse the data

--- Part_09_NASA_Project/server/src/app.js
+++ Part_09_NASA_Project/server/src/app.js
@@ -5,5 +5,8 @@ const express = require('express');
 // creating a server by help of express 
 const app = express();
 
+// For parsing the incomming data of JSON type from user side 
+app.use(express.json());
+
 // exporting this module to server.js
 module.exports = app;
