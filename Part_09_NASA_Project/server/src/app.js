8b6b4b1a5901bafc98baef75ba13b63bc4667903
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-08 16:38:41 +0530

    serving frontend by backend by use of build method

--- Part_09_NASA_Project/server/src/app.js
+++ Part_09_NASA_Project/server/src/app.js
@@ -1,6 +1,7 @@
 // Load the Express library to create a web server and handle routes
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -14,6 +15,15 @@ app.use(cors({
 
 // So that the data could be treated as a json file
 app.use(express.json());
+
+// Code to use and run the react frontend from this server -->> By using the app.use(express.static()) function 
+// we will be serving the public folder that contains all the information to load the frontend
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Now we need to router our project to http://localhost:8000/index.html
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 // Starting to use our first server
 app.use(planetsRouter);
