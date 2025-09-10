988dfdf0af8c77be43990f4b9a49c8d09955f96d
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-10 18:12:24 +0530

    updated

--- Part_09_NASA_Project/server/src/routes/launches/launches.controller.js
+++ Part_09_NASA_Project/server/src/routes/launches/launches.controller.js
@@ -6,7 +6,7 @@ const {
 } = require('../../models/launches.model');
 
 // Creating the getAllLaunches function so that we could access all the launches data 
-function httpGetAllLaunches(req, res) {
+function httpGetAllLaunches(req, res) {  
 
     // Geeting the data from launches by launches.values() and converting it to array by Array.from() and treating it as json 
     return res.status(200).json(getAllLaunches());
