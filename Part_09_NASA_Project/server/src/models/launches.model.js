1bd6a18db9cf8fee86d5ad6fb36a08e245627c7b
Sourav Kumar <xsouravkumar357@gmail.com>
2025-09-10 15:33:11 +0530

    Updated the addNewLaunch Function

--- Part_09_NASA_Project/server/src/models/launches.model.js
+++ Part_09_NASA_Project/server/src/models/launches.model.js
@@ -2,6 +2,9 @@
 // we could store the data of the satelites in many ways -->> but we will use the map data structure as it is very good 
 const launches = new Map();
 
+// Storing the latest Flight Number 
+let latestFlighNumber = 100;
+
 // creating a object of lauch 
 const launch = {
     filghtNumber: 100,
@@ -9,7 +12,7 @@ const launch = {
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
     destination: 'kepler-442 b',
-    cutomer: ['ZTM', 'NASA'],
+    customer: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
 };
@@ -18,8 +21,28 @@ const launch = {
 // we can get all data of the satelite
 launches.set(launch.filghtNumber, launch);
 
+// Function to get all launches 
+function getAllLaunches() {
+    return Array.from(launches.values());
+}
+
+// Creating a function to add new launch 
+function addNewLaunch(launch) {
+    latestFlighNumber++;
+    launches.set(
+        latestFlighNumber, 
+        Object.assign(launch, {
+            filghtNumber: latestFlighNumber,
+            customer: ['Zero to Mastery', 'NASA'],
+            upcoming: true,
+            success: true,
+        })
+    )
+}
+
 // exporting the module data now so that it could be used elsewhere
 module.exports = {
-    launches,
+    getAllLaunches,
+    addNewLaunch,
 };
 
