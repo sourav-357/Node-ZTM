8f442245d1407cc3c6f66ee207f6e16b8c45c8d6
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-05 07:38:31 +0530

    added a new file for schema

--- Part_09_NASA_Project/server/src/models/launches.mongo.js
+++ Part_09_NASA_Project/server/src/models/launches.mongo.js
@@ -4,5 +4,10 @@ const mongoose = require('mongoose');
 
 // stroring the schema 
 const launchesSchema = new mongoose.Schema({
+    flightNumber: {
+        type: Number,
+        required: true,
+    },
+    LaunchDate: Date,
     
 })
