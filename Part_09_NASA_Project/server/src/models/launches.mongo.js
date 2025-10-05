1ca1fe0f74fc701408ca531102786908773c1f7b
Sourav Kumar <xsouravkumar357@gmail.com>
2025-10-05 07:56:35 +0530

    schema for every property

--- Part_09_NASA_Project/server/src/models/launches.mongo.js
+++ Part_09_NASA_Project/server/src/models/launches.mongo.js
@@ -9,5 +9,30 @@ const launchesSchema = new mongoose.Schema({
         required: true,
     },
     LaunchDate: Date,
-    
-})
+    missions: {
+        type: String,
+        required: true,
+    },
+    rocket: {
+        type: String,
+        required: true,
+    },
+    target: {
+        type: String,
+        required: true,
+    },
+    customer: [ String ],
+    upcoming: {
+        type: Boolean,
+        require: true,
+    },
+    success: {
+        type: Boolean,
+        require: true,
+    },
+    umcoming: {
+        type: Boolean,
+        require: true,
+        default: true,
+    }
+});
